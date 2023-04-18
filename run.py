5bae0f9495b811d2e40265c695e1f9979075c58b
Katerina <yelkaterina@gmail.com>
2023-04-18 18:19:43 +0200

    Edit all docstrings

--- run.py
+++ run.py
@@ -27,18 +27,23 @@ final_q = df[['SurveyLength', 'SurveyEase']]
 
 def display_menu():  
     """
-    Display menu of available options of the programm.
-    The user can choose to see survey results,
-    cross-tab analysis, edit column names or exit the program.
+    Display the menu of available program options for the user.
+
+    Uses the 'tabulate' library to display menu options: survey results, cross-tab analysis, 
+    edit columns and exit the program. Prompts the user to select an option by entering a number 
+    1 through 4 and validates the input to ensure that it's integer between 1 and 4.
+
+    Returns:
+        int: the user's validated choice of menu option
     """
+
     list_length = range(1, 5)
+
     print('\n' *2)
     print('\t\tMENU')
-    print(tabulate([['1- ', 'Survey results'], ['2- ', 'Cross-tab Analysis'], ['3- ', 'Edit Column names'], ['4- ', 'Exit']]))
-    # user_choice = int(input('Choose option number 1, 2, 3 or 4:\n'))
+    print(tabulate([['1- ', 'Survey Results'], ['2- ', 'Cross-tabulation Analysis'], ['3- ', 'Edit Column Names'], ['4- ', 'Exit']]))
     user_choice = validate_input(list_length)
     
-
     if user_choice == 1:
         main()
 
@@ -57,18 +62,18 @@ def display_menu():
 
 def validate_input(lists):
     """
-    Prompts the user to enter a number, validates the input, 
-    and returns the input if it is valid.
+    Prompts the user to enter a number, validates that it is a valid integer and within the range
+    of possible indices for the given list, returns the input. If the input is invalid, the 
+    function prompts the user until the valid input is received.
 
     Parameters:
-        lists: a list of any data type; used to set maximum length of the displayed lists. 
+        list: a list of any data type. Used to set the range of indices of the given list.
     Returns:
-        int: the validated input from the user
+        int: the validated input from the user.
     Raises:
         ValueError: if input values cannot be converted into an integer
         UnboundLocalError: if input values are outside of the range of possible indices for
         the given list.
-
     """
 
     max_value = len(lists)
@@ -91,17 +96,21 @@ def validate_input(lists):
 
 def display_sections():
     """
-    Display the enumerated sections, take the user's choice of a section;
-    Display the enumerated questions, take the user's choice of a question;
+    Display the enumerated sections.
+
+    Displays all survey sections. Prompts the user to select an option by entering a number 
+    1 through 6 and validates the input to ensure that it is an integer between 1 and 6.
+
+    Returns:
+        int: the user's validated choice of section number.
     """
 
     list_length = range(1, 7)   
+
     print('\nSECTIONS:\n')
     print(' 1- Basic Information\n 2- Education, Work, and Career\n 3- Technology and Tech Culture\n 4- Stack Overflow Usage + Community\n 5- Demographic Information\n 6- Final Questions\n')
-    # section_number = int(input('Enter Section number:\n'))
     section_number = validate_input(list_length)                
              
-
     if section_number == 1:
         choice = basic_info
             
@@ -118,18 +127,25 @@ def display_sections():
         choice = demograph
             
     elif section_number == 6:
-        choice = final_q                    
+        choice = final_q                
     
     
     return choice
     
 
 
-
 def display_questions(section):
     """
-    Display the list of questions of the chosen
-    section.
+    Display the enumerated list of section's questions.
+
+    Displays all questions of the chosen section. Prompts the user to select a question by entering a number 
+    of the question and validates the input to ensure that it is an integer within the allowed range. Assigns
+    the chosen integer to the column name of the DataFrame.
+
+    Parameters:
+        section (int): validated section number.
+    Returns:
+        question_id (str): the user's choice of question as a column name of the DataFrame.
     """
 
     print('\nQUESTIONS:\n')
@@ -137,23 +153,27 @@ def display_questions(section):
     result = section.columns.tolist()
     for i, column in enumerate(result):
         print(f'{i+1}- {column}')
-
-    # question_num = int(input('\nEnter Question number\n'))
+    
     question_num = validate_input(result)
 
     # subtract 1 to get to zero-based index
     question_id = result[question_num -1] 
     
-    print(f'You have chosen {question_id}')
+    print(f'You have chosen {question_id}')   
     return question_id
     
 
 
 def display_survey_results(question):
     """
-    Take result from the user's input, check if 
-    the question is multi- or single-answer,
-    display first 15 survey results for the chosen question
+    Display results of the chosen question.
+    
+    Displays first 15 survey results for the chosen question.
+    Check if the chosen question is multi- or single-answer type. If the question is the multi-answer
+    type, the function splits the answers and then display results.
+
+    Parameters:
+        question (str): DataFrame column name.    
     """
     
     print('\n' *3)
@@ -169,8 +189,11 @@ def display_survey_results(question):
 
 def cross_tab():
     """
-    Allow the user to select two datasets 
-    for cross-tabulation chart
+    Allow the user to select two datasets for cross-tabulation.
+
+    Displays options to choose - section number and question number for dataset 1 and dataset 2. 
+    Check if the chosen datasets contain multi- or single-answer questions. Displays cross-tabulation 
+    results. Displays the main menu.
     """
      
     print('\n\t\tChoose DATASET 1\n')
@@ -183,7 +206,7 @@ def cross_tab():
 
     print('\n' *3)
     print('\t\tRESULTS\n')
-    # display all columns of crosstab
+    # display all columns of the cross-tab
     pd.set_option('display.max_columns', None)
 
     if df[question_group1].str.contains(';').any() and df[question_group2].str.contains(';').any(): 
@@ -212,8 +235,16 @@ def cross_tab():
 
 def change_column_name(df, old_name):    
     """
-    Allow the user to edit or rename column names and 
-    display updated list of all columns.
+    Allow the user to edit or rename DataFrame columns.
+     
+    Prompts the user to enter a new name for the specified DataFrame column. 
+    Renames the column using rename() pandas method. 
+    Displays the updated list of all DataFrame columns.
+    Displays the main menu.
+
+    Parameters:
+        df (pd.DataFrame): the DataFrame to modify.
+        old_name (str): the name of the column to be modified.
     """
     
     new_name = input(f'Enter new column name for {old_name}\n')
@@ -226,16 +257,19 @@ def change_column_name(df, old_name):
 
 
 def main():
-    
+    """
+    Run all program functions.
+    """
     user_section = display_sections()
     user_question = display_questions(user_section)
     display_survey_results(user_question)
     display_menu()
-    
+
 
 
 print('\t\t2021 Stack Overflow Developer Survey')
 display_menu()
+
 
 
 
